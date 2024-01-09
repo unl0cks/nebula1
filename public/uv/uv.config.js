6996d83cb8cd5a1b6b3c501f63ccc2f7a5c8d88f
Percy <percy@railway.app>
2024-01-09 17:14:05 +0000

    Initial Commit

---
+++ public/uv/uv.config.js
@@ -0,0 +1,11 @@
+self.__uv$config = {
+  prefix: "/~/uv/",
+  bare: "/bare/",
+  encodeUrl: Ultraviolet.codec.aes.encode,
+  decodeUrl: Ultraviolet.codec.aes.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js"
+};
